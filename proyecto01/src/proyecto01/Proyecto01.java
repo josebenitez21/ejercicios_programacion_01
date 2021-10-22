5a96c357dcc9bce2769e7f901f3a3202264af7bd
Jose Benitez <jlbenitez4@utpl.edu.ec>
2021-10-22 10:49:38 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Generando un miniprograma en Java");
+        System.out.println("Usando Netbeans");
+        System.out.println("junto");
+        System.out.println("Git y GitHub");
+
+    }
+
+}
